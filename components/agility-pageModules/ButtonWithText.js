133ee092446766d3008c7c71c63c981d67d06427
ClydeError <peterlishman18@gmail.com>
2022-04-07 18:48:26 +0300

    Add 404 Page for mobile version and Some fix with burger menu

--- components/agility-pageModules/ButtonWithText.js
+++ components/agility-pageModules/ButtonWithText.js
@@ -5,12 +5,11 @@ const ButtonWithText = ({module})=>{
 console.log(fields)
 
     return(
-      <div className={""}>
-          <div className={"max-w-screen-xl mx-auto flex flex-col content-center space-y-5 mt-[11%]"}>
-              <h3 className={"max-w-[900px] self-center text-center md:px-[20px]"}>{fields.title}</h3>
-              <p className={"b1 w-[350px] text-center self-center md:px-[20px]"}>{fields.text}</p>
-              <a href={fields.button.href} className="bttn1 text-white w-[180px] h-[56px] rounded-full bg-primary-blue text-center py-3 self-center ">{fields.button.text}</a>
-
+      <div className={"md:bg-[#F0F9FF]"}>
+          <div className={"max-w-screen-xl mx-auto flex flex-col content-center space-y-5 mt-[11%] md:pb-[165px] md:bg-[#F0F9FF]"}>
+              <h3 className={"max-w-[900px] self-center text-center md:px-[20px] md:mt-[150px] md:text-[30px] md:leading-[2rem] md:text-primary-darkblue"}>{fields.title}</h3>
+              <p className={"b1 w-[350px] text-center self-center md:px-[20px] md:mt-[1px] md:text-[16px]"}>{fields.text}</p>
+              <a href={fields.button.href} className="bttn1 text-white w-[180px] h-[56px] rounded-full bg-primary-blue text-center py-3 md:py-3 self-center md:w-[156px] md:h-[56px] md:text-[14px] ">{fields.button.text}</a>
           </div>
       </div>
     );
