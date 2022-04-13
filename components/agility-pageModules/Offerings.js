4b12426cf7545c292654ad9e224ae2d389262b7d
Artsiom Masiakin <yrcraf@gmail.com>
2022-04-13 16:11:18 +0300

    AORN Offerings fix

--- components/agility-pageModules/Offerings.js
+++ components/agility-pageModules/Offerings.js
@@ -4,12 +4,12 @@ import React from 'react'
 const CourseItem = ({data})=> {
     console.log(data)
     return(
-        <div className={"py-10"}>
-            <div className={"bg-agility w-11/12 h-80 rounded-xl text-white text-center"}>
-                {!data.image ? "image should be here" : <img className={"rounded-xl"} src={data.image.url}/>}
+        <div className={"py-10 w-[640px]"}>
+            <div className={`h-[320px] w-[640px] rounded-xl text-white text-center ${!data.image ? 'bg-agility' : ''} `}>
+                {!data.image ? "image should be here" : <img className={"rounded-xl"} src={data.image.url} width={"640px"} height={"320px"}/>}
             </div>
-            <div className={"b3 py-4"}>({new Date(data.date).getFullYear()}) (ITEM # {data.id})</div>
-            <div className={"c1"}>{data.name}</div>
+            <div className={"b3 py-4"}>({new Date(data.startDate).getFullYear()}) (ITEM # {data.id})</div>
+            <div className={"c1 w-[640px]"}>{data.name}</div>
         </div>
     )
 }
@@ -20,7 +20,7 @@ const CourseBlock = ({blockData})=>{
             <h3>
                 {blockData[0]}
             </h3>
-            <div className={"grid grid-cols-2 py-1 mb-5 "}>
+            <div className={"flex flex-wrap justify-between  py-1 mb-5"}>
                 {blockData[1].map((e,i)=>{
                     return(
                         <CourseItem data={e.fields}/>
@@ -37,6 +37,7 @@ const Offerings = ({module})=>{
     const course = {}
     fields.courses.map((e)=>{course[e.fields.filter] ? course[e.fields.filter].push(e) : course[e.fields.filter] = [e] })
     const courseArr = Object.entries(course)
+    console.log(courseArr)
 
     return(
         <div>
@@ -45,14 +46,15 @@ const Offerings = ({module})=>{
                     {fields.title}
                 </h1>
                 <div className={"b1 py-3"}>View our current curriculum.</div>
-                <div className={"relative space-x-3 py-2 mb-6 "}>
-                    <div className={"inline-block p-1 border-2 border-secondary-blue bg-secondary-blue rounded-3xl px-5 py-2 b3 "}>All</div>
-                    <div className={"inline-block p-1 border-2 border-secondary-blue rounded-3xl px-5 py-2 b3 "}>Aseptic practices</div>
-                    <div className={"inline-block p-1 border-2 border-secondary-blue rounded-3xl px-5 py-2 b3 "}>Equipment and product safety</div>
+                <div className={"flex overflow-hidden"}>
+                <div className={"flex space-x-4 py-2 mb-6 w-full flex-nowrap "}>
+                    <div className={"flex-initial border-2 border-secondary-blue rounded-3xl b3 h-[42px] cursor-pointer " +
+                        "py-2 px-2 flex-shrink-0 text-center min-w-[55px]"}>All</div>
+                    {courseArr.map((e)=>
+                        <div className={"flex-initial border-2 border-secondary-blue rounded-3xl b3 h-[42px] py-2 px-2" +
+                            " flex-shrink-0 text-center cursor-pointer"}>{e[0]}</div>)}
 
-
-
-
+                </div>
                 </div>
                 {
                     courseArr.map((e,i)=>{
