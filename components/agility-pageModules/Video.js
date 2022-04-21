f1096499e89576c68aa964e1a9ef649457515642
ClydeError <peterlishman18@gmail.com>
2022-04-21 14:37:27 +0300

    add page mobile page About US 4/7 modules

--- components/agility-pageModules/Video.js
+++ components/agility-pageModules/Video.js
@@ -7,8 +7,8 @@ const Video = ({module}) => {
         background:`linear-gradient(${fields.bgTop} 50%, ${fields.bgBot} 50%)`
     }
     return(
-        <div className={"flex max-w-full justify-center py-12"} style={bgGradient}>
-            <div className={"rounded-2xl"} dangerouslySetInnerHTML={renderHTML(fields.video)}/>
+        <div className={"flex max-w-full justify-center py-12 md:px-5"} style={bgGradient}>
+            <div className={"rounded-2xl md:w-full"} dangerouslySetInnerHTML={renderHTML(fields.video)}/>
         </div>
     );
 
