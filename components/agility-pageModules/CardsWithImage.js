62c02b8bd8f1d46baaaf9a9f9a1d9aafbbb64c65
ClydeError <peterlishman18@gmail.com>
2022-04-29 19:37:56 +0300

    Fix CardsWithImage

--- components/agility-pageModules/CardsWithImage.js
+++ components/agility-pageModules/CardsWithImage.js
@@ -3,7 +3,8 @@ import React from "react"
 
 const Card = ({iconUrl, title, text, iconBg, cardBg, textClr}) => {
     return(
-        <div className={"border-2 md:w-[335px] mr-4 mb-4 border-agility card p-4 pr-10 space-y-3 rounded-xl max-w-md last:odd:max-w-max flex-grow lg:h-40 inline-block lg:justify-center align-middle "} style={{backgroundColor: cardBg, color: textClr}}>
+        <div className={"border-2 md:w-[335px] lg:mb-[29px]  border-agility card p-4 pr-10 space-y-2 rounded-xl max-w-md " +
+            "last:odd:max-w-max flex-grow lg:h-40 inline-block lg:justify-center align-middle "+`${imgPos == "right" ? "mr-4" : "ml-4"}` } style={{backgroundColor: cardBg, color: textClr}}>
             {iconUrl ?
                 <img className={"rounded"} src={iconUrl} height={"48"} width={"48"} style={{backgroundColor: iconBg}} />
                 : <div className={"w-12 h-12"}/>}
@@ -23,12 +24,12 @@ const CardsWithImage = ({module}) =>{
     return(
        <div className={"lg:max-w-screen-xl mx-auto md:mx-5 "}>
            <h1 className={"my-16 mobile"}>{fields.title}</h1>
-           <div className={"c1 pb-1 "}>{fields.caption}</div>
-           <div className={"lg:grid lg:grid-cols-2 space-y-4"}>
-               <div className={`lg:-translate-y-20 ${fields.imagePosition == "right" ? 'order-last' : '' }`}>
+           <div className={"c1 pb-1 relative z-50 translate-y-6 "}>{fields.caption}</div>
+           <div className={"lg:grid lg:grid-cols-2 space-y-4 " + `${fields.imagePosition == "right" ? 'space-x-0' : 'space-x-0' }`}>
+               <div className={`lg:-translate-y-12 ${fields.imagePosition == "right" ? 'order-last ml-4' : 'mr-4' }`}>
                    <img className={"rounded-xl"} src={fields.image.url}/>
                </div>
-               <div className={"flex flex-wrap lg:justify-center md:justify-center"}>
+               <div className={"flex flex-wrap lg:justify-center md:justify-center lg:content-center " + `${fields.imagePosition == "right" ? '' : '' }`}>
 
                {fields.cards.slice(0).reverse().map((card, index) => {
 
