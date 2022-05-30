8f193bbde4361f81be1dc1bcaa14285bc84c55a7
Artsiom Masiakin <yrcraf@gmail.com>
2022-05-30 16:03:10 +0300

    cron yml WITH COURSE SAVE

--- pages/api/course-sync.js
+++ pages/api/course-sync.js
@@ -1,4 +1,21 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import agilityMgmt from '@agility/content-management'
+
+//Create a new instance API client
+const api = agilityMgmt.getApi({
+    location: 'USA',
+    websiteName: 'CineMed',
+    securityKey: 'QwBpAG4AZQBNAGUAZAAtADYAQgA2AEEAMABFAEEAOAAtAEYANQBBADkALQA0AEIAQQA5AC0AOQAwAEYAMgAtADIAMABDADUANwBEAEYAQQA0AEUAQgA5AA=='
+});
+let contentItem = {
+    contentID: -1,
+    fields: {
+        "Time": "Test time",
+        "Type": "test type"
+    }
+}
+let languageCode = "en-us";
+let referenceName = "testsync";
+
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
@@ -6,6 +23,22 @@ export default async function handler(req, res) {
             const { authorization } = req.headers;
 
             if (authorization === `Bearer ${process.env.AGILITY_SECURITY_KEY}`) {
+
+                api.saveContentItem({
+                    contentItem,
+                    languageCode,
+                    referenceName
+                })
+                    .then(function(contentID) {
+                        //check contentID is greater > 0 for success
+                           //update contentID of saved item
+                        console.log(contentID)
+                        console.log(`contentID`)
+                    })
+                    .catch(function(error) {
+                        //handle error
+                    });
+
                 res.status(200).json({ success: true });
             } else {
                 res.status(401).json({ success: false });
